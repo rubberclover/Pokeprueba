e7eebe6d53ad0d71c5b3525723c5afad106bf6e9
rubberclover <marlite98@gmail.com>
2024-06-24 17:06:41 +0900

    Added New Pokemon

--- src/main/java/command/Pokedex.java
+++ src/main/java/command/Pokedex.java
@@ -59,7 +59,7 @@ public class Pokedex implements AutoCloseable{
 		            "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy");
 	      byte[] image;
 	      try {
-		      for (int i=1; i<18; i++) {
+		      for (int i=0; i<19; i++) {
 		    	  image = Files.readAllBytes(Paths.get("images/type/"+typeNames.get(i)+".png"));
 		    	  loadTypeIfNotExists(transaction, i, typeNames.get(i), image);
 		      }
@@ -74,15 +74,72 @@ public class Pokedex implements AutoCloseable{
 		byte[] image;
 		try {
 		    image = Files.readAllBytes(Paths.get("images/pokemon/001_Bulbasaur.png"));
-		    loadPokemonIfNotExists(transaction, 001,"Bulbasaur", image);
-	      } catch (IOException e) {
+		    loadPokemonIfNotExists(transaction, 1,"Bulbasaur",1,5,8,0.7,6.9,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/004_Charmander.png"));
+		    loadPokemonIfNotExists(transaction, 4,"Charmander",1,2,0,0.6,8.5,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/007_Squirtle.png"));
+		    loadPokemonIfNotExists(transaction, 7,"Squirtle",1,3,0,0.5,9,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/010_Caterpie.png"));
+		    loadPokemonIfNotExists(transaction, 10,"Caterpie",1,12,0,0.3,2.9,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/016_Pidgey.png"));
+		    loadPokemonIfNotExists(transaction, 16,"Pidgey",1,1,10,0.3,1.8,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/023_Ekans.png"));
+		    loadPokemonIfNotExists(transaction, 23,"Ekans",1,8,0,2,6.9,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/025_Pikachu"));
+		    loadPokemonIfNotExists(transaction, 25,"Pikachu",1,4,0,0.4,6,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/035_Clefairy"));
+		    loadPokemonIfNotExists(transaction, 35,"Clefairy",1,19,0,0.6,7.5,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/050_Diglett"));
+		    loadPokemonIfNotExists(transaction, 50,"Diglett",1,9,0,0.2,0.8,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/063_Abra"));
+		    loadPokemonIfNotExists(transaction, 63,"Abra",1,11,0,0.9,19.5,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/066_Machop"));
+		    loadPokemonIfNotExists(transaction, 66,"Machop",1,7,0,0.8,19.5,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/074_Geodude"));
+		    loadPokemonIfNotExists(transaction, 74,"Geodude",1,13,9,0.4,20,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/092_Gastly"));
+		    loadPokemonIfNotExists(transaction, 92,"Gastly",1,14,8,1.3,0.1,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/147_Dratiny"));
+		    loadPokemonIfNotExists(transaction, 147,"Dratiny",1,16,0,1.8,3.3,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/198_Murkrow"));
+		    loadPokemonIfNotExists(transaction, 198,"Murkrow",2,17,10,0.5,2.1,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/215_Sneasel"));
+		    loadPokemonIfNotExists(transaction, 215,"Sneasel",2,17,6,0.9,28,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/216_Teddiursa"));
+		    loadPokemonIfNotExists(transaction, 216,"Teddiursa",2,1,0,0.6,8.8,image);
+		    image = Files.readAllBytes(Paths.get("images/pokemon/227_Scarmory"));
+		    loadPokemonIfNotExists(transaction, 227,"Scarmory",2,18,10,1.7,50.5,image);
+	      }
+		catch (TransactionException e) {
+	    	  throw e;
+	      } 
+		catch (IOException e) {
 		      throw e;
 	      }
 		
 	}
 
-	private void loadPokemonIfNotExists(DistributedTransaction transaction, int i, String string, byte[] image) {
-		// TODO Auto-generated method stub
+	private void loadPokemonIfNotExists(DistributedTransaction transaction, int id, String name,int generation,int type1,int type2,double height,double weight, byte[] image) throws TransactionException {
+		Optional<Result> type = transaction.get(
+			   	Get.newBuilder()
+				.namespace("pokedex")
+				.table("pokemon")
+				.partitionKey(Key.ofInt("pokemon_id", id))
+				.clusteringKey(Key.of("type1", type1,"type2", type2))
+				.build());
+			if (!type.isPresent()) {
+				transaction.put(
+					Put.newBuilder()
+					.namespace("pokedex")
+					.table("pokemon")
+					.partitionKey(Key.ofInt("pokemon_id", id))
+					.clusteringKey(Key.of("type1", type1,"type2", type2))
+					.intValue("generation", generation)
+					.doubleValue("height", height)
+					.doubleValue("weight", weight)
+					.blobValue("image", image)
+					.build());
+			}
 		
 	}
 
