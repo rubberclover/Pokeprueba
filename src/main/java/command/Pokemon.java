8df7302ec72e4894a15f6f476b3f3745320d5cdc
rubberclover <marlite98@gmail.com>
2024-06-27 17:55:01 +0900

    Added new methods to Pokemon and weekness

--- src/main/java/command/Pokemon.java
+++ src/main/java/command/Pokemon.java
@@ -5,6 +5,7 @@ import com.scalar.db.api.DistributedTransactionManager;
 import com.scalar.db.api.Put;
 import com.scalar.db.api.Result;
 import com.scalar.db.api.Scan;
+import com.scalar.db.exception.transaction.AbortException;
 import com.scalar.db.exception.transaction.TransactionException;
 import com.scalar.db.io.Key;
 import com.scalar.db.service.TransactionFactory;
@@ -14,6 +15,7 @@ import javafx.collections.ObservableList;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Pokemon {
 
@@ -83,15 +85,104 @@ public class Pokemon {
 	}
   }  */
 
-	public List<Result> getPokemonsFiltered(Map<String, ObservableList<String>> filterSelections) {
+	public List<Result> getPokemonsFiltered(Map<String, ObservableList<String>> filterSelections) throws TransactionException{
 		// Convert ObservableList into List of string and make search of filters
 		// Weaknesses should be called in Weakness.java to find the types
-		return null;
+		Weakness weakness = new Weakness("scalardb.properties");
+		DistributedTransaction tx = manager.start();
+		try {
+		    Scan scan =
+		    Scan.newBuilder()
+		        .namespace(NAMESPACE)
+		        .table(TABLENAME)
+		        .all()
+		        .build();
+		    List<Result> results = tx.scan(scan);  
+			// Commit the transaction*/
+			tx.commit();
+			Set<String> keys = filterSelections.keySet();
+			for(String key : keys) {
+				switch(key) {
+					case "Generation":
+						for(String filterCommand : filterSelections.get(key)) {
+							results.removeIf(e -> e.getInt("generation") != Integer.parseInt(filterCommand));
+						};
+						break;
+					case "Type":
+						for(String filterCommand : filterSelections.get(key)) {
+							results.removeIf(e -> e.getInt("type1") != Integer.parseInt(filterCommand) || e.getInt("type2") != Integer.parseInt(filterCommand));
+						};
+						break;
+					case "Height":
+						for(String filterCommand : filterSelections.get(key)) {
+							if(filterCommand.contains("-")) {
+							String[] arrOfStr = filterCommand.split("-", 2);
+					        arrOfStr[1] = arrOfStr[1].replace("m", "");
+							results.removeIf(e -> e.getDouble("height") < Double.parseDouble(arrOfStr[0]) || e.getDouble("height") > Double.parseDouble(arrOfStr[1]));
+								}
+								else {
+									String[] arrOfStr = filterCommand.split("+", 2);
+									results.removeIf(e -> e.getDouble("height") < Double.parseDouble(arrOfStr[0]));
+								}
+							};
+						break;
+					case "Weight":
+						for(String filterCommand : filterSelections.get(key)) {
+							if(filterCommand.contains("-")) {
+								String[] arrOfStr = filterCommand.split("-", 2);
+						        arrOfStr[1] = arrOfStr[1].replace("kg", "");
+								results.removeIf(e -> e.getDouble("weight") < Double.parseDouble(arrOfStr[0]) || e.getDouble("weight") > Double.parseDouble(arrOfStr[1]));
+									}
+									else {
+										String[] arrOfStr = filterCommand.split("+", 2);
+										results.removeIf(e -> e.getDouble("weight") < Double.parseDouble(arrOfStr[0]));
+									};
+						};
+						break;
+					case "Weaknesses":
+						List<Result> weakTo = weakness.getWeaknessByTypes(filterSelections.get(key));
+						results.removeIf(e -> weakTo.contains(e.getInt("type1")) == false || weakTo.contains(e.getInt("type2")) == false);
+						break;
+					default:
+		                break;
+						}
+				
+			}
+			return results;
+		
+		    } catch (Exception e) {
+		     	tx.abort();
+		    	throw e;
+		    }
+		
 	}
 	
-	public List<Result> getPokemonByNameOrId(String search) {
+	public List<Result> getPokemonByNameOrId(String search) throws TransactionException {
+		DistributedTransaction tx = manager.start();
+	    try {
+	    Scan scan =
+	    Scan.newBuilder()
+	        .namespace(NAMESPACE)
+	        .table(TABLENAME)
+	        .all()
+	        .build();
+	    List<Result> results = tx.scan(scan);    
+		// Commit the transaction*/
+		tx.commit();
+		if(search.matches("-?\\d+(\\.\\d+)?")){
+			results.removeIf(e -> e.getInt("pokemon_id") != Integer.parseInt(search));
+		}
+		else
+		{
+		results.removeIf(e -> e.getText("name") != search);
+		}
+		return results;
+	
+	    } catch (Exception e) {
+	     	tx.abort();
+	    	throw e;
+	    }
 		
-		return null;
 	}
 
 	public void close() {
--- src/main/java/command/Weakness.java
+++ src/main/java/command/Weakness.java
@@ -9,7 +9,12 @@ import com.scalar.db.api.Scan;
 import com.scalar.db.exception.transaction.TransactionException;
 import com.scalar.db.io.Key;
 import com.scalar.db.service.TransactionFactory;
+
+import javafx.collections.ObservableList;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -52,9 +57,104 @@ public class Weakness {
 			throw e;
 		}
 	}
+	
+	public List<Result> getWeaknessByTypes(ObservableList<String> types) throws TransactionException {
+		List<String> typeNames = Arrays.asList(
+				  "None", "Normal", "Fire", "Water","Electric", "Grass", "Ice", 
+		            "Fighting", "Poison", "Ground", "Flying", "Psychic", 
+		            "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy");
+		// Start a transaction
+		DistributedTransaction tx = manager.start();
+		try {
+			Scan scan =
+				    Scan.newBuilder()
+				        .namespace(NAMESPACE)
+				        .table(TABLENAME)
+				        .all()
+				        .build();
+			List<Result> results =  tx.scan(scan);  
+			List<Integer> idTypes = new ArrayList<>();  
+			for(String type : types) {
+				int typeInt = typeNames.indexOf(type);
+				idTypes.add(typeInt);
+			}
+			results.removeIf(e -> idTypes.contains(e.getInt("weakness_id")) == false);
+		    // Commit the transaction
+		    tx.commit();
+		
+		    return results;
+		} catch (Exception e) {
+			tx.abort();
+			throw e;
+		}
+	}
   // Method to get weaknesses for 2 types
-  
-  // Method to calcul weakness for 2 types
+	
+	public List<Result> getWeaknessByTypes(Integer type1, Integer type2) throws TransactionException {
+		// Start a transaction
+		DistributedTransaction tx = manager.start();
+		
+		try {
+			Scan scan =
+				    Scan.newBuilder()
+				        .namespace(NAMESPACE)
+				        .table("weakness")
+				        .all()
+				        .build();
+			List<Result> endResults = new ArrayList<>();  
+			List<Result> results =  tx.scan(scan);  
+			List<Result> results2 =  tx.scan(scan); 
+			results.removeIf(e -> e.getInt("weakness_id") != type1);
+			results2.removeIf(e -> e.getInt("weakness_id") != type2);
+			for(Result res: results) {
+				for(Result res2: results2) {
+					if(res.getInt("attacker_type") == res2.getInt("attacker_type") & (res.getDouble("mult") * res2.getDouble("mult")) >= 2.0) {
+						endResults.add(res);
+					}
+				}
+			}
+		    // Commit the transaction
+		    tx.commit();
+		
+		    return endResults;
+		} catch (Exception e) {
+			tx.abort();
+			throw e;
+		}
+	}
+	
+	public List<Result> getWeaknessNonEffectiveByTypes(Integer type1, Integer type2) throws TransactionException {
+		// Start a transaction
+		DistributedTransaction tx = manager.start();
+		
+		try {
+			Scan scan =
+				    Scan.newBuilder()
+				        .namespace(NAMESPACE)
+				        .table("weakness")
+				        .all()
+				        .build();
+			List<Result> endResults = new ArrayList<>();  
+			List<Result> results =  tx.scan(scan);  
+			List<Result> results2 =  tx.scan(scan); 
+			results.removeIf(e -> e.getInt("weakness_id") != type1);
+			results2.removeIf(e -> e.getInt("weakness_id") != type2);
+			for(Result res: results) {
+				for(Result res2: results2) {
+					if(res.getInt("attacker_type") == res2.getInt("attacker_type") & (res.getDouble("mult") * res2.getDouble("mult")) <= 0.5) {
+						endResults.add(res);
+					}
+				}
+			}
+		    // Commit the transaction
+		    tx.commit();
+		
+		    return endResults;
+		} catch (Exception e) {
+			tx.abort();
+			throw e;
+		}
+	}
   
   // Method to get attack for 1 type
   
